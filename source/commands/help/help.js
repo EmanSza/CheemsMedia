f2a05aeab13ebad82a659921f53172b54661f210
EmanSza <eszabadka@hotmail.ca>
2021-02-06 22:58:34 -0500

    Updated file profile needs working on
    TypeError: Cannot read property '_id' of null when using another userrs ID
    
    Needs Schema / DB updated

---
+++ source/commands/help/help.js
@@ -0,0 +1,67 @@
+const PREFIX = require('../../../config/botconfig.json').PREFIX;
+const { MessageEmbed } = require("discord.js");
+
+
+module.exports = {
+    name: 'help',
+    aliases: ["h"],
+    description: "Get help on commands.",
+    cooldown: 0,
+    usage: `To get help on a specific command, use \`${PREFIX}help [command name]\` (without the [ ]).\nFor a full list of all commands, simply use \`${PREFIX}help\`.`,
+    examples: `\`${PREFIX}help ping\``,
+    canNotDisable: true,
+    
+    execute: async function(client, message, args) {
+        if (!args.length) {
+            let hEmbed = new MessageEmbed()
+            .setTitle("Commands")
+            .setURL("https://discord.gg/jRnTbRPjdr")
+            .setColor("RANDOM")
+            .setDescription(`Use \`${PREFIX}help [command name]\` to get more info on a specific command, for example: \`${PREFIX}help ping\``)
+            .setThumbnail(client.user.displayAvatarURL())  
+
+
+            let AwEmbed = new MessageEmbed()
+            .setTitle("Help Commands have Been Sent!")
+            .setColor("RANDOM")
+
+            let MIEmbed = new MessageEmbed()
+            .setURL("https://discord.gg/jRnTbRPjdr")
+            .setColor(EMBED_COLOR)
+            .setTitle("Commands")
+            .addFields(
+                {name: "**Misc**", value:"``ping`` See how fast the bot is\n ``up`` give cheems to a image\n ``down`` take a cheem away from the image", inline: true},
+                {name: "**Media**", value:"``post`` post a image\n ``profile`` see your profile statistics\n ``random`` get a random image\n", inline: true},
+             )
+             PEmbed = new MessageEmbed()
+                .setColor(EMBED_COLOR)
+                .setTitle("Premium Commands")
+
+
+            message.channel.send(AwEmbed).then
+            setTimeout(function() {
+                message.author.send(hEmbed)
+            }, 500).then
+            setTimeout(function() {
+                message.author.send(MIEmbed)
+            }, 501)
+        }
+        else {
+            const cmdname = args[0].toLowerCase();
+            const command = client.commands.get(cmdname) || client.commands.find(c => c.aliases && c.aliases.includes(cmdname));
+
+            if (!command) return message.channel.send(`${message.author.username}, that\'s not a valid command!`)
+
+            let hEmbed = new MessageEmbed()
+            .setTitle(`${command.name}`)
+            .setDescription(`${command.description}`)
+            .setColor(EMBED_COLOR)
+            .setTimestamp()
+            if (command.usage) hEmbed.addField("Usage", `${command.usage}`)
+            if (command.aliases && command.aliases.length !== 0) hEmbed.addField("Aliases", `${command.aliases.join(', ')}`)
+            if (command.examples) hEmbed.addField("Examples", `${command.examples}`)
+            if (client.guildInfoCache.get(message.guild.id).disabledCommands.includes(command.name)) hEmbed.setAuthor('This command is currently disabled in this server.')
+            message.channel.send(hEmbed);
+        }
+    }
+}
--- source/commands/media/profile.js
+++ source/commands/media/profile.js
@@ -12,23 +12,37 @@ module.exports = {
     cooldown: 0,
 
     execute: async function(client, message, args) {
-        let DBUser = await client.DBUser.findById(message.author.id);
+
+        let user = client.users.cache.get(args[0]) || message.mentions.users.first()
+        if(!user) user = message.author.id
+      //  if (user.id === message.author.id) return
+
+        let DBUser = await client.DBUser.findById(user);
     if (!DBUser) {
-        const fetch = await client.DBUser.findById(message.author.id, { new: true, upsert: true });
+        const fetch = await client.DBUser.findById(user, { new: true, upsert: true });
+        // When testing when making the 1st argument into a ID got a id of null pushing into dev branch
         DBUser['_id'] = fetch._id
         DBUser['cheems'] = fetch.cheems
         DBUser['posts'] = fetch.posts.length
+        DBUser['followers'] = fetch.followers
+        DBUser['follows'] = fetch.follows
     }
     const cheems = DBUser.cheems;
-    const userid = DBUser._id;
     const postlength = DBUser.posts.length
+    const followers = DBUser.followers
+    const follows = DBUser.follows
+    const Totalfollows = DBUser.follows.length
 
 
     const profile = new MessageEmbed()
     .setTitle(message.author.tag)
     .addFields(
         {name: "Total Posts:", value: postlength, inline: false },
-        {name: "Total Cheems", value: cheems, inline: false }
+        {name: "Total Cheems", value: cheems, inline: true },
+        // Didnt have time to update the DB to get defaults
+       // {name: "Total Followers", value: followers, inline: false },
+       // {name: "Total Follows", value: Totalfollows, inline: false },
+       // {name: "Following", value: follows, inline: false }
     )
     .setThumbnail(message.author.displayAvatarURL())
     message.channel.send(profile)
