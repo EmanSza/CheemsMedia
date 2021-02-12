89c71fb413577732c7e3f149ee3cf4dbd2c81c33
EmanSza <eszabadka@hotmail.ca>
2021-02-12 17:01:03 -0500

    Update help.js

--- source/commands/help/help.js
+++ source/commands/help/help.js
@@ -10,42 +10,42 @@ module.exports = {
     usage: `To get help on a specific command, use \`${PREFIX}help [command name]\` (without the [ ]).\nFor a full list of all commands, simply use \`${PREFIX}help\`.`,
     examples: `\`${PREFIX}help ping\``,
     canNotDisable: true,
-
-    execute: async function (client, message, args) {
+    
+    execute: async function(client, message, args) {
         if (!args.length) {
             let hEmbed = new MessageEmbed()
-                .setTitle("Help Menu!")
-                .setURL("https://discord.gg/jRnTbRPjdr")
-                .setColor("RANDOM")
-                .setDescription(`Use \`${PREFIX}help [command name]\` to get more info on a specific command, for example: \`${PREFIX}help ping\` \n Dont Forget to use the \`signup\` command to signup for our bot! `)
-                .setThumbnail(client.user.displayAvatarURL({ dynamic: true, size: 2048 }))
+            .setTitle("Commands")
+            .setURL("https://discord.gg/jRnTbRPjdr")
+            .setColor("RANDOM")
+            .setDescription(`Use \`${PREFIX}help [command name]\` to get more info on a specific command, for example: \`${PREFIX}help ping\``)
+            .setThumbnail(client.user.displayAvatarURL())  
 
 
             let AwEmbed = new MessageEmbed()
-                .setTitle("Help Commands have Been Sent!")
-                .setColor("RANDOM")
+            .setTitle("Help Commands have Been Sent!")
+            .setColor("RANDOM")
 
             let MIEmbed = new MessageEmbed()
-                .setURL("https://discord.gg/jRnTbRPjdr")
-                .setColor('RANDOM')
-                .setTitle("Commands")
-                .addFields(
-                    { name: "**Misc**", value: "`ping` See how fast the bot is\n`up` give cheems to a image\n`down` take a cheem away from the image", inline: true },
-                    { name: "**Media**", value: "`post` post a image\n`profile` see your profile statistics\n`random` get a random image\n`follow` Follow someone\n`unfollow` unfollow someone\n", inline: true },
-                )
-            PEmbed = new MessageEmbed()
-                .setColor('RANDOM')
+            .setURL("https://discord.gg/jRnTbRPjdr")
+            .setColor(EMBED_COLOR)
+            .setTitle("Commands")
+            .addFields(
+                {name: "**Misc**", value:"``ping`` See how fast the bot is\n ``up`` give cheems to a image\n ``down`` take a cheem away from the image", inline: true},
+                {name: "**Media**", value:"``post`` post a image\n ``profile`` see your profile statistics\n ``random`` get a random image\n", inline: true},
+             )
+             PEmbed = new MessageEmbed()
+                .setColor(EMBED_COLOR)
                 .setTitle("Premium Commands")
                 try{
                 await message.author.send(hEmbed)
                 await message.author.send(MIEmbed)
                 await message.channel.send(AwEmbed)
         } catch {
+            
+            message.channel.send("Woops your DM's are closed")
 
             message.channel.send(hEmbed)
             message.channel.send(MIEmbed)
-
-            message.channel.send("Woops your DM's are closed")
 
         }
         }
@@ -56,10 +56,10 @@ module.exports = {
             if (!command) return message.channel.send(`${message.author.username}, that\'s not a valid command!`)
 
             let hEmbed = new MessageEmbed()
-                .setTitle(`${command.name}`)
-                .setDescription(`${command.description}`)
-                .setColor("RANDOM")
-                .setTimestamp()
+            .setTitle(`${command.name}`)
+            .setDescription(`${command.description}`)
+            .setColor(EMBED_COLOR)
+            .setTimestamp()
             if (command.usage) hEmbed.addField("Usage", `${command.usage}`)
             if (command.aliases && command.aliases.length !== 0) hEmbed.addField("Aliases", `${command.aliases.join(', ')}`)
             if (command.examples) hEmbed.addField("Examples", `${command.examples}`)
