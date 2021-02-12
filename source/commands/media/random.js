ce9de5f980a7f0ce7eb0b86a71037c81fc1368a6
EmanSza <eszabadka@hotmail.ca>
2021-02-12 17:02:33 -0500

    Update random.js

--- source/commands/media/random.js
+++ source/commands/media/random.js
@@ -1,6 +1,6 @@
-const PREFIX = require('../../../config/botconfig.json').PREFIX;
+const PREFIX = require('../../../../../config/botconfig.json').PREFIX;
 const { MessageEmbed } = require('discord.js')
-const { getReply } = require('../../utils/utils')
+const { getReply } = require('../../../../utils/utils')
 // Change DIR if needed
 
 module.exports = {
@@ -8,35 +8,31 @@ module.exports = {
     description: "Get a random post",
     usage: `\`${PREFIX}random\``,
 
-    execute: async function (client, message, args) {
-        let DBUser = await client.DBUser.findById(message.author.id);
-
+    execute: async function(client, message, args) {
         let posts = await client.DBPost.find({});
         let post = posts.random()
-        if(!post) return message.channel.send("There are no posts on this guild :thinking:")
+
         // return console.log(post)
         const user = await client.users.fetch(post.author);
 
         const embed = new MessageEmbed()
-            .setColor('RANDOM')
-            .setTitle(post.title)
-            .setDescription(post.description)
-            .setFooter(`${user.tag} | ${user.id}`, user.displayAvatarURL({ dynamic: true }));
+        .setColor('RANDOM')
+        .setTitle(post.title)
+        .setDescription(post.description)
+        .setFooter(user.tag, user.displayAvatarURL({ dynamic: true }));
 
-        if (post.image !== 'none') embed.setImage(post.image);
+        if (post.image !== 'None') embed.setImage(post.image);
 
         message.channel.send(embed)
 
         let reply = await getReply(message, { time: 30000 })
         if (!reply) return;
         if (reply.content.toLowerCase() === '!up') {
-            if (!DBUser) return message.reply('In order to cheems you must signup!');
             await client.DBPost.findByIdAndUpdate(post._id, { $inc: { upvotes: 1 } }, { new: true, upsert: true });
             await client.DBUser.findByIdAndUpdate(post.author, { $inc: { cheems: 1 } }, { new: true, upsert: true });
             message.reply('Cheems Given! 😊')
         }
         else if (reply.content.toLowerCase() === '!down') {
-            if (!DBUser) return message.reply('In order to cheems you must signup');
             await client.DBPost.findByIdAndUpdate(post._id, { $inc: { downvotes: 1 } }, { new: true, upsert: true });
             await client.DBUser.findByIdAndUpdate(post.author, { $inc: { cheems: -1 } }, { new: true, upsert: true });
             message.reply('Cheems Taken! 😢')
