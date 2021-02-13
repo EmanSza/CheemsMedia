6f267c33bfd93155b052e03f3b44a5cb2d66357b
EmanSza <eszabadka@hotmail.ca>
2021-02-13 14:50:47 -0500

    Edited colors :flushed:

--- source/commands/help/help.js
+++ source/commands/help/help.js
@@ -13,13 +13,6 @@ module.exports = {
 
     execute: async function (client, message, args) {
         if (!args.length) {
-            let hEmbed = new MessageEmbed()
-                .setTitle("Help Menu!")
-                .setURL("https://discord.gg/jRnTbRPjdr")
-                .setColor("RANDOM")
-                .setDescription(`Use \`${PREFIX}help [command name]\` to get more info on a specific command, for example: \`${PREFIX}help ping\` \n Dont Forget to use the \`signup\` command to signup for our bot! `)
-                .setThumbnail(client.user.displayAvatarURL({ dynamic: true, size: 2048 }))
-
 
             let AwEmbed = new MessageEmbed()
                 .setTitle("Help Commands have Been Sent!")
@@ -27,25 +20,22 @@ module.exports = {
 
             let MIEmbed = new MessageEmbed()
                 .setURL("https://discord.gg/jRnTbRPjdr")
-                .setColor('RANDOM')
-                .setTitle("Commands")
+                .setColor('#FD0061')
+                .setTitle("Help Menu")
+                .setDescription(`Use \`${PREFIX}help [command name]\` to get more info on a specific command, for example: \`${PREFIX}help ping\` \n Dont Forget to use the \`signup\` command to signup for our bot! `)
+                .setThumbnail(client.user.displayAvatarURL({ dynamic: true, size: 2048 }))
                 .addFields(
                     { name: "**Misc**", value: "`ping` See how fast the bot is\n`up` give cheems to a image\n`down` take a cheem away from the image", inline: true },
-                    { name: "**Post**", value: "`post` post a image\n`totalpost` See all the post of a user!\n`viewpost` View a Certain post by ID\n`deletepost` Delete one of your images!\n`random` View a Random post!\n`editdescription` Edit a post description!", inline: true },
+                    { name: "**Post**", value: "`post` post a image\n`totalpost` See all the post of a user!\n`viewpost` View a Certain post by ID\n`deletepost` Delete one of your posts!!\n`random` View a Random post!\n`editdescription` Edit a post description!", inline: true },
                     { name: "**Guild**", value: "`channelfeed` Set the Guilds Feed\n`feeduser` Make the Guild Follow a user!\n`unfeedchannel` unSet the Guilds Feed\n`unfeeduser` Make the Guild unFollow a user!\n", inline: true },
                     { name: "**User**", value: "`signup` Create a Cheems Media Account!\n`block` Block a user!\n`unblock` unblock a user!\n`follow` Follow a user!\n`unfollow` unFollow a user!\n", inline: true },
-                    { name: "**Misc**", value: "`Profile` View yours or a users Profile!\n`setColor` See set your profiles color!\n`setbio`Set your profiles description!\n", inline: true }
+                    { name: "**Profile**", value: "`Profile` View yours or a users Profile!\n`setColor` See set your profiles color!\n`setbio`Set your profiles description!\n", inline: true }
                 )
-            PEmbed = new MessageEmbed()
-                .setColor('RANDOM')
-                .setTitle("Premium Commands")
                 try{
-                await message.author.send(hEmbed)
                 await message.author.send(MIEmbed)
                 await message.channel.send(AwEmbed)
         } catch {
             message.channel.send("Woops your DM's are closed so im Sending it here!")
-            message.channel.send(hEmbed)
             message.channel.send(MIEmbed)
 
         }
