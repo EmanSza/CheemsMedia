773f674ddfcf51de9d3fb6beab7d56919bfa3afc
EmanSza <eszabadka@hotmail.ca>
2021-02-15 18:17:59 -0500

    Added Checked for Database Managers

--- config/schema/posts.js
+++ config/schema/posts.js
@@ -16,6 +16,10 @@ const postSchema = Schema({
     image: String,
     upvotes: Number,
     downvotes: Number,
+    checked: {
+        type: String,
+        default: "unchecked"
+    },
     comments: commentSchema
 })
 
