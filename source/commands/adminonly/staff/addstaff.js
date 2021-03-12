9ac400cdab11efdac3e5b8fbd657daa849810ad7
Barçın <68463413+lbarcl@users.noreply.github.com>
2021-03-13 00:10:47 +0300

    Update addstaff.js

--- source/commands/adminonly/staff/addstaff.js
+++ source/commands/adminonly/staff/addstaff.js
@@ -15,7 +15,7 @@ module.exports = {
         return staff._id === message.mentions.users.first().id
       })
 
-      if (staff[0].name) return message.reply(`${staff.name} is already registered as ${staff.job}`)
+      if (staff[0].name) return message.reply(`${staff[0].name} is already registered as ${staff[0].job}`)
 
       await new client.DBStaff({
         _id: message.mentions.users.first().id,
