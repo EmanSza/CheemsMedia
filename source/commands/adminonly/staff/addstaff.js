3bd883403ca56c2d0e717a91e5cf484bfca0cbbc
Barçın <68463413+lbarcl@users.noreply.github.com>
2021-03-13 00:32:19 +0300

    test commit #1

--- source/commands/adminonly/staff/addstaff.js
+++ source/commands/adminonly/staff/addstaff.js
@@ -9,13 +9,15 @@ module.exports = {
     someServersOnly: true,
 
     execute: async function(client, message, args) {
-      if(message.author.id != '519861424017768451') return message.reply(`Only <@519861424017768451> can add staff`)
+      if(message.author.id != '455831445949120535') return message.reply(`Only <@519861424017768451> can add staff`)
       if(!message.mentions.users.first()) return message.reply('You need to mention user for staff `!addstaff <mention> <job>` ')
       if(!args[1]) return message.reply('You need to enter job for staff `!addstaff <mention> <job>` ')
+      console.log(client.stafflist)
       const staff = client.stafflist.filter(function(staff){
         return staff._id === message.mentions.users.first().id
       })
-
+      console.log(staff)
+      
       if (staff[0].name) return message.reply(`${staff[0].name} is already registered as ${staff[0].job}`)
 
       await new client.DBStaff({
