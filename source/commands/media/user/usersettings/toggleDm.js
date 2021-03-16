c062f5c22f5a00ca7bbddc66734643a7a888e037
EmanSza <eszabadka@hotmail.ca>
2021-03-16 10:34:55 -0400

    Added Changes to ToggleDM Feature

--- source/commands/media/user/usersettings/toggleDm.js
+++ source/commands/media/user/usersettings/toggleDm.js
@@ -11,14 +11,15 @@ module.exports = {
     cooldown: 10,
 
     execute: async function(client, message, args) {
-        let DBUser = await client.DBUser.findById(message.author.id);
+        let author = message.author
+        let DBUser = await client.DBUser.findById(author.id);
         if (!DBUser) return message.reply('You must signup using the signup command!');
         
         if(DBUser.dmOpt)  {
-            await client.DBUser.findByIdAndUpdate(message.author.id, { $set: { dmOpt: false  } }, { new: true, upsert: true });
+            await client.DBUser.findByIdAndUpdate(author.id, { $set: { dmOpt: false  } }, { new: true, upsert: true });
             message.reply('I have disabled you\`re DMs')
         } else /* If dmOpt is false */ {
-            await client.DBUser.findByIdAndUpdate(message.author.id, { $set: { dmOpt: true  } }, { new: true, upsert: true });
+            await client.DBUser.findByIdAndUpdate(author.id, { $set: { dmOpt: true  } }, { new: true, upsert: true });
             message.reply('I have enabled you\`re DMs')
         }
     }
