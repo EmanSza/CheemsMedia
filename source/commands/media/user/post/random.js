acb1a755614e1ca442c7fa91fbd51dc2ed3ec6ae
lbarcl <ersoybarcin@gmail.com>
2021-03-19 01:14:14 +0300

    voting fixed

--- source/commands/media/user/post/random.js
+++ source/commands/media/user/post/random.js
@@ -9,7 +9,7 @@ module.exports = {
     usage: `\`${PREFIX}random\``,
     cooldown: 5,
 
-    execute: async function(client, message, args) {
+    execute: async function(client, message) {
         let posts = await client.DBPost.find({});
         let post = nsfwPost(message.channel, posts)
         if(post.checked != true) await client.DBPost.find({});
@@ -21,29 +21,31 @@ module.exports = {
         .setColor('RANDOM')
         .setTitle(post.title)
         .setDescription(post.description)
-        .setFooter(user.tag, user.displayAvatarURL({ dynamic: true }));
+        .setFooter(user.tag + ` Post id: ${post._id}`, user.displayAvatarURL({ dynamic: true }));
 
         if (post.image.toLowerCase() !== 'none') embed.setImage(post.image);
         message.channel.send(embed)
 
         let reply = await getReply(message, { time: 30000 })
         if (!reply) return;
-        if (reply.content.toLowerCase() === '!up') {
-            if(DBPost.cheemGivers.includes(message.author.id)) return message.reply('You have already given Cheems to this post');
-            if(DBPost.cheemTakers.includes(message.author.id)) { await client.DBPost.findByIdAndUpdate(DBPost._id, { $pull: { cheemTakers: message.author.id } }, { new: true, upsert: true}); }
-              await client.DBPost.findByIdAndUpdate(DBPost._id, { $inc: { upvotes: 1 } }, { new: true, upsert: true });
-              await client.DBUser.findByIdAndUpdate(DBPost.author, { $inc: { cheems: 1 } }, { new: true, upsert: true });
-              await client.DBPost.findByIdAndUpdate(DBPost._id, { $push: { cheemGivers: message.author.id } }, { new: true, upsert: true});
-              message.reply('Cheems Given! 😊')
+        if (reply.content.toLowerCase() == '!up') {
+          if(post.cheemGivers.includes(message.author.id)) return message.reply('You have already given Cheems to this post');
+          if(post.cheemTakers.includes(message.author.id)) { 
+            await client.DBPost.findByIdAndUpdate(post._id, { $pull: { cheemTakers: message.author.id } }, { new: true, upsert: true}); 
           }
-          else if (reply.content.toLowerCase() === '!down') {
-            if(DBPost.cheemTakers.includes(message.author.id)) return message.reply('You have already taken Cheems to this post');
-            if(DBPost.cheemGivers.includes(message.author.id)) { await client.DBPost.findByIdAndUpdate(DBPost._id, { $pull: { cheemGivers: message.author.id } }, { new: true, upsert: true}); }
-              await client.DBPost.findByIdAndUpdate(DBPost._id, { $inc: { downvotes: 1 } }, { new: true, upsert: true });
-              await client.DBUser.findByIdAndUpdate(DBPost.author, { $inc: { cheems: -1 } }, { new: true, upsert: true });
-              await client.DBPost.findByIdAndUpdate(DBPost._id, { $push: { cheemTakers: message.author.id } }, { new: true, upsert: true});
-              message.reply('Cheems Taken! 😢')
-          }
+          await client.DBPost.findByIdAndUpdate(post._id, { $inc: { upvotes: 1 } }, { new: true, upsert: true });
+          await client.DBUser.findByIdAndUpdate(post.author, { $inc: { cheems: 1 } }, { new: true, upsert: true });
+          await client.DBPost.findByIdAndUpdate(post._id, { $push: { cheemGivers: message.author.id } }, { new: true, upsert: true});
+          message.reply('Cheems Given! 😊')
+        }
+        else if (reply.content.toLowerCase() == '!down') {
+          if(post.cheemTakers.includes(message.author.id)) return message.reply('You have already taken Cheems to this post');
+          if(post.cheemGivers.includes(message.author.id)) { await client.DBPost.findByIdAndUpdate(post._id, { $pull: { cheemGivers: message.author.id } }, { new: true, upsert: true}); }
+            await client.DBPost.findByIdAndUpdate(post._id, { $inc: { downvotes: 1 } }, { new: true, upsert: true });
+            await client.DBUser.findByIdAndUpdate(post.author, { $inc: { cheems: -1 } }, { new: true, upsert: true });
+            await client.DBPost.findByIdAndUpdate(post._id, { $push: { cheemTakers: message.author.id } }, { new: true, upsert: true});
+            message.reply('Cheems Taken! 😢')
+        }
     }
 }
 
--- source/commands/media/user/post/upvote.js
+++ source/commands/media/user/post/upvote.js
@@ -6,28 +6,27 @@ module.exports = {
     name: "upvote",
     aliases: [],
     description: "",
-    usage: `\`${PREFIX}\``,
-    examples: `\`${PREFIX}\``,
+    usage: `\`${PREFIX}upvote [post-id]\``,
+    examples: `\`${PREFIX}upvote 822220305644388362\``,
     perms: [],
     cooldown: 5,
 
     execute: async function(client, message, args) {
       let postID = args[0];
       if (!postID) {
-      await message.channel.send('Please send me the ID');
+      message.channel.send('Please send me the ID');
       const reply = await getReply(message, { time: 30000});
       if (reply && reply.content) postID = reply.content;
       else return message.channel.send('I need a valid ID')
       }
 
-        let DBPost = await client.DBPost.findById(postID.content)
-        if(!DBPost) return(`Could not Fetch a post with the id ${postID.content}`)
-
-          if(DBPost.cheemGivers.includes(message.author.id)) return message.reply('You have already taken Cheems to this post');
-          if(DBPost.cheemTakers.includes(message.author.id)) { await client.DBPost.findByIdAndUpdate(DBPost._id, { $pull: { cheemTakers: message.author.id } }, { new: true, upsert: true}); } 
-            await client.DBPost.findByIdAndUpdate(DBPost._id, { $inc: { upvotes: 1 } }, { new: true, upsert: true });
-            await client.DBUser.findByIdAndUpdate(DBPost.author, { $inc: { cheems: 1 } }, { new: true, upsert: true });
-            await client.DBPost.findByIdAndUpdate(DBPost._id, { $push: { cheemTakers: message.author.id } }, { new: true, upsert: true});
-            message.reply('Cheems Taken! 😢')
+      let DBPost = await client.DBPost.findById(postID)
+      if(!DBPost) return(`Could not Fetch a post with the id ${postID}`)
+      if(DBPost.cheemGivers.includes(message.author.id)) return message.reply('You have already taken Cheems to this post');
+      if(DBPost.cheemTakers.includes(message.author.id)) { await client.DBPost.findByIdAndUpdate(DBPost._id, { $pull: { cheemTakers: message.author.id } }, { new: true, upsert: true}); } 
+        await client.DBPost.findByIdAndUpdate(DBPost._id, { $inc: { upvotes: 1 } }, { new: true, upsert: true });
+        await client.DBUser.findByIdAndUpdate(DBPost.author, { $inc: { cheems: 1 } }, { new: true, upsert: true });
+        await client.DBPost.findByIdAndUpdate(DBPost._id, { $push: { cheemTakers: message.author.id } }, { new: true, upsert: true});
+        message.reply('Cheems Given! 😊')
     }
 }
