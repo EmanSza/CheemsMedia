5a73b0e36e60b3aa603a0414681fc4f1edc4c19b
Ethan Szabadka <eszabadka@hotmail.ca>
2022-04-01 23:10:50 -0400

    Emptied Branch
    Branch has been emptied, a new codebase will be created.
    Due to the rushed state of Cheems it is in our best interest to restart

--- config/schema/config.js
+++
@@ -1,8 +0,0 @@
-const { Schema, model } = require('mongoose');
-const { PREFIX } = require('../botconfig.json')
-
-const config = Schema({
-    _id: String
-}, { strict: false });
-
-module.exports = model('config', config);
